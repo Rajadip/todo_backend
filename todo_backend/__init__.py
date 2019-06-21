281e2685164a79477083ff238f6bc51073361c45
Rajadip Patil <rajadippatil@gmail.com>
2019-06-21 20:02:28 +0530

    added instance folder

--- todo_backend/__init__.py
+++ todo_backend/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_pyfile('config.py')
 
     return app
