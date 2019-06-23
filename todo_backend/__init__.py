7de91cf50f1b313b690d5cf4613d062d816aa1f5
Rajadip Patil <rajadippatil@gmail.com>
2019-06-23 12:04:38 +0530

    enabled cross origin

--- todo_backend/__init__.py
+++ todo_backend/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from todo_backend.extension import *
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_pyfile('config.py')
 
+    cors.init_app(app=app)
     with app.app_context():
         from todo_backend.route import todo_blueprint
         app.register_blueprint(todo_blueprint)
